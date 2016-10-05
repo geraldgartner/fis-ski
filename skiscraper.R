da9a52a348ccc521ec2610fa5f86238bed4c6c8c
“[geraldgartner]git config --global user.email “[Gerald.Gartner@gmail.com] <[gerald.gartner@gmail.com]>
2016-10-05 22:17:24 +0200

    update until failed data scraping

--- skiscraper.R
+++ skiscraper.R
@@ -7,7 +7,7 @@ library(dplyr)
 result = NULL
 
 #set to desired years
-for (i in seq(2015, 2016)) {
+for (i in seq(1967, 2016)) {
   main.page1 <- read_html(paste("http://data.fis-ski.com/global-links/all-fis-results.html?place_search=&sector_search=AL&date_search=&gender_search=&category_search=WC&codex_search=&nation_search=&disciplinecode_search=&date_from=01&search=Search&limit=1000&seasoncode_search=", i, sep = ""))
   
   tmp = main.page1 %>% # feed `main.page` to the next step
@@ -29,22 +29,24 @@ colnames(result)[1] <- "url"
 
 scrapeSubpages <- function(x) {
   tabletest <- read_html(as.character(x)) %>% 
-    html_node("table") %>%
-    html_table(header = TRUE)
+    html_node("table.calendar") %>%
+    html_table(header = TRUE, fill = TRUE)
+  tabletest$url = x
   tabletest
     }
 
 
 subresults <- mapply(scrapeSubpages, result$url)
+
 subresults <- do.call(cbind.data.frame, subresults)
+subresults <- subset(subresults, select=-"Downloads")
 #df <- data.frame(matrix(unlist(subresults), nrow=69, byrow=T),stringsAsFactors=FALSE)
 
+#refine scrape table attempt
 
-#SUBPAGE TESTING - WORKS
-subpagetest <- read_html("http://data.fis-ski.com/dynamic/event-details.html?event_id=2687&cal_suchsector=AL")
+refine <- read_html("http://data.fis-ski.com/dynamic/event-details.html?event_id=2687&cal_suchsector=AL") %>% 
+  html_node("table.calendar") %>%
+  html_table(header = TRUE, fill = TRUE)
+refine
 
-subtest <- subpagetest %>% 
-  html_nodes("td a.view-fiche") %>%
-  html_attr("href") %>%
-  as.data.frame()
-subtest
+write.csv(result, file = "result.csv")
