83ac2fc652c818540aab9ee19ee732c917156d78
“[geraldgartner]git config --global user.email “[Gerald.Gartner@gmail.com] <[gerald.gartner@gmail.com]>
2016-10-14 17:34:57 +0200

    update

---
+++ worldcupstanding_analysis.R
@@ -0,0 +1,24 @@
+#Load Libraries
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+
+data <- read.csv("worldcup_standings_mf.csv")
+
+data_wc <- subset(data, wc_all_rank=="1")
+
+data_wc_m_seqd <- data_wc_m[,seq(2, nrow(data_wc_m), 2)]
+
+dummy <- 1
+
+
+wc_all_m <- ggplot(data_wc_m, aes(x = dummy, y = year,fill=nation)) +
+  geom_tile(stat="identity", color="white")+
+  facet_wrap(~nation)
+
+print(wc_all_m)
+
+alltitles <- ggplot(data_wc, aes(x=year, y=nation, fill=nation))+
+  geom_bar(stat="identity", color="white", width=0.6)+
+  facet_wrap(~gender, nrow=1)
+print(alltitles)
